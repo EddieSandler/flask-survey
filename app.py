0b3f5b094055e2699a137844071857a44b9e54f7
Eddie Sandler <ed.sandler@gmail.com>
2023-10-11 16:19:14 -0400

    initial commit- need to capture answers in list

---
+++ app.py
@@ -0,0 +1,67 @@
+from flask import Flask,request,render_template
+from surveys import satisfaction_survey
+# from flask_debugtoolbar import DebugToolbarExtension
+
+
+app = Flask(__name__)
+# app.debug = True
+app.config['SECRET_KEY'] = 'I got this'
+app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
+
+# toolbar = DebugToolbarExtension(app)
+responses = []
+
+@app.route('/')
+def get_survey():
+    title=satisfaction_survey.title
+    instructions=satisfaction_survey.instructions
+
+    return render_template('base.html',title=title,instructions=instructions)
+
+@app.route('/questions/0')
+def show_question1():
+
+    this_question=satisfaction_survey.questions[0].question
+    print(request.args)
+
+
+    return render_template('question1.html',this_question=this_question)
+
+
+@app.route('/questions/1')
+def show_question2():
+
+    this_question=satisfaction_survey.questions[1].question
+
+
+    return render_template('question2.html',this_question=this_question)
+
+@app.route('/questions/2')
+def show_question3():
+
+    this_question=satisfaction_survey.questions[2].question
+    choice_1=satisfaction_survey.questions[2].choices[0]
+    choice_2=satisfaction_survey.questions[2].choices[1]
+
+
+
+
+    return render_template('question3.html',this_question=this_question,choice1=choice_1,choice2=choice_2)
+
+@app.route('/questions/3')
+def show_question4():
+
+    this_question=satisfaction_survey.questions[3].question
+
+
+
+    return render_template('question4.html',this_question=this_question)
+
+@app.route('/result')
+def show_results():
+
+
+    survey_responses= responses
+
+
+    return render_template('result.html',responses=survey_responses)
