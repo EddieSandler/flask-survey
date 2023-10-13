4e90b2fff2226d7a77608a06cac8156a1a60610e
Eddie Sandler <ed.sandler@gmail.com>
2023-10-13 11:33:40 -0400

    assignment completed

--- app.py
+++ app.py
@@ -1,70 +1,80 @@
-from flask import Flask,request,render_template
-from surveys import satisfaction_survey
-# from flask_debugtoolbar import DebugToolbarExtension
+from flask import Flask, request, render_template, redirect, flash, session
+from flask_debugtoolbar import DebugToolbarExtension
+from surveys import satisfaction_survey as survey
 
+# key names will use to store some things in the session;
+# put here as constants so we're guaranteed to be consistent in
+# our spelling of these
+RESPONSES_KEY = "responses"
 
 app = Flask(__name__)
-# app.debug = True
-app.config['SECRET_KEY'] = 'I got this'
+app.config['SECRET_KEY'] = "never-tell!"
 app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 
-# toolbar = DebugToolbarExtension(app)
-responses = []
-
-@app.route('/')
-def get_survey():
-    title=satisfaction_survey.title
-    instructions=satisfaction_survey.instructions
-
-    return render_template('base.html',title=title,instructions=instructions)
-
-@app.route('/questions/0',methods=['POST','GET'])
-def show_question1():
-
-    this_question=satisfaction_survey.questions[0].question
+debug = DebugToolbarExtension(app)
 
 
-    return render_template('question1.html',this_question=this_question)
+@app.route("/")
+def show_survey_start():
+    """Select a survey."""
+
+    return render_template("survey_start.html", survey=survey)
 
 
-@app.route('/questions/1',methods=['POST','GET'])
-def show_question2():
+@app.route("/begin", methods=["POST"])
+def start_survey():
+    """Clear the session of responses."""
 
-    this_question=satisfaction_survey.questions[1].question
+    session[RESPONSES_KEY] = []
 
-
-
-    return render_template('question2.html',this_question=this_question)
-
-@app.route('/questions/2')
-def show_question3():
-
-    this_question=satisfaction_survey.questions[2].question
-    choice_1=satisfaction_survey.questions[2].choices[0]
-    choice_2=satisfaction_survey.questions[2].choices[1]
+    return redirect("/questions/0")
 
 
+@app.route("/answer", methods=["POST"])
+def handle_question():
+    """Save response and redirect to next question."""
+
+    # get the response choice
+    choice = request.form['answer']
+
+    # add this response to the session
+    responses = session[RESPONSES_KEY]
+    responses.append(choice)
+    session[RESPONSES_KEY] = responses
+
+    if (len(responses) == len(survey.questions)):
+        # They've answered all the questions! Thank them.
+        return redirect("/complete")
+
+    else:
+        return redirect(f"/questions/{len(responses)}")
 
 
+@app.route("/questions/<int:qid>")
+def show_question(qid):
+    """Display current question."""
+    responses = session.get(RESPONSES_KEY)
+
+    if (responses is None):
+        # trying to access question page too soon
+        return redirect("/")
+
+    if (len(responses) == len(survey.questions)):
+        # They've answered all the questions! Thank them.
+        return redirect("/complete")
+
+    if (len(responses) != qid):
+        # Trying to access questions out of order.
+        flash(f"Invalid question id: {qid}.")
+        return redirect(f"/questions/{len(responses)}")
+
+    question = survey.questions[qid]
+    return render_template(
+        "question.html", question_num=qid, question=question)
 
 
-    return render_template('question3.html',this_question=this_question,choice1=choice_1,choice2=choice_2)
+@app.route("/complete")
+def complete():
+    """Survey complete. Show completion page."""
 
-@app.route('/questions/3',methods=['POST','GET'])
-def show_question4():
-
-    this_question=satisfaction_survey.questions[3].question
-  
-
-
-
-    return render_template('question4.html',this_question=this_question)
-
-@app.route('/result',methods=['POST','GET'])
-def show_results():
-
-
-    survey_responses= responses
-
-
-    return render_template('result.html',responses=survey_responses)
+    return render_template("completion.html")
